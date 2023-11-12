6c01634b897b6cc907f67ad6b48355726aad7eab
sarmueller <sarah@mueteam.de>
2023-11-12 12:50:28 +0100

    uncomment image download

--- download_data/download.sh
+++ download_data/download.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#mkdir -p data/images                        
-#cd data/images                           
-#curl -sS https://www5.cs.fau.de/fileadmin/research/datasets/fundus-images/healthy.zip > images.zip 
-#unzip images.zip                                  
-#rm images.zip
+mkdir -p data/images                        
+cd data/images                           
+curl -sS https://www5.cs.fau.de/fileadmin/research/datasets/fundus-images/healthy.zip > images.zip 
+unzip images.zip                                  
+rm images.zip
 
 cd data/images
 cd ..
